8619cc0f68b927e0f600f70cb1e620b4b325852f
emerson <escanorrosa123@gmail.com>
2023-06-02 12:22:58 -0500

    hola

--- routes/datePayments.routes.js
+++ routes/datePayments.routes.js
@@ -5,6 +5,6 @@ const datePaymentsController = require('../controllers/datePayments.controller')
 const router = express.Router();
 
 router.get('/allDatePayments', datePaymentsController.findAll);
-router.get('/oneDatePayments/id', datePaymentsController.findOne);
+router.get('/oneDatePayments/:id', datePaymentsController.findOne);
 
 module.exports = router;
